179cdad4311408ae8be0c0e17c24fbd667c806b4
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-06-13 21:39:52 -0400

    deleted some unnecessary comments

--- notes-rest/src/main/java/com/Component/notesrest/NoteController.java
+++ notes-rest/src/main/java/com/Component/notesrest/NoteController.java
@@ -10,43 +10,33 @@ import java.util.List;
         @Autowired
         private NoteRepository noteRepository;
 
-        //    Request: GET /ping
-//    Response: "Up"
+
         @GetMapping("/ing")
         public String ing(){
             return "Up";
         }
 
-        //    Request: GET /expenses
+
 
         @GetMapping("/getAllnotes")
         public List<Note> getAllnotes(){
         return noteRepository.findAll();
         }
 
-//            Request: GET /expense/{id}
-//    Response: Expense
-//    @GetMapping("/notes/{id}")
-//    public Note getNotesById(@PathVariable int id){
-//        return noteRepository.findById(id);
-//    }
 
-        //    Request: POST /expense Expense
-//    Response: Expense
+
         @PostMapping("/notes")
         public Note saveNote(@RequestBody Note note){
             return noteRepository.save(note);
         }
 
-        //    Request: DELETE /expense/{id}
-//    Response: void
+
         @DeleteMapping("/getnote/{id}")
         public void deleteNoteById(@PathVariable int id){
             noteRepository.deleteById(id);
         }
 
-        //    Request: DELETE /expense Expense
-//    Response: void
+
         @DeleteMapping("/getnote")
         public void deleteNote(@RequestBody Note note){
             noteRepository.delete(note);
